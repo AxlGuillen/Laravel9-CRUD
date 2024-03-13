541ff4cd275c0e33ed53616cbf1904be98f4a081
AxlGuillen <axl1504@hotmail.com>
2024-03-12 20:10:18 -0600

    Parrafo en about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,4 +6,7 @@
 
 @section('content')
     <h1 class="my-4 font-serif text-3xl text-center text-sky-600 dark:text-sky-500">About</h1>
+    <p>
+        Non ad mollit elit laboris ad. Sit do adipisicing qui deserunt cillum et velit culpa adipisicing nisi laboris esse do. Incididunt sint excepteur id qui nisi do anim veniam aliqua ipsum. Tempor do minim anim consequat cillum esse sint reprehenderit laboris incididunt magna ipsum est nisi. Sit nostrud dolor sit incididunt pariatur laborum veniam. Tempor consectetur sit do minim aliquip do proident. Consectetur aliquip sit proident nisi irure.
+    </p>
 @endsection
