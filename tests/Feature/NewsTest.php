70819c35ce532d5efeb60cc1d109f73013f209be
AxlGuillen <axl1504@hotmail.com>
2024-04-07 23:07:12 -0600

    Formato correguido

--- tests/Feature/NewsTest.php
+++ tests/Feature/NewsTest.php
@@ -41,70 +41,69 @@ class NewsTest extends TestCase
     }
 
     /** @test */
-public function a_user_can_view_a_single_news_item()
-{
-    $newsItem = News::create([
-        'title' => 'Sample News',
-        'resume' => 'This is a sample news item.',
-        'author' => 'Author Name',
-    ]);
+    public function a_user_can_view_a_single_news_item()
+    {
+        $newsItem = News::create([
+            'title' => 'Sample News',
+            'resume' => 'This is a sample news item.',
+            'author' => 'Author Name',
+        ]);
 
-    $response = $this->get('/news/'.$newsItem->id);
-    $response->assertStatus(200)
-             ->assertSee($newsItem->title)
-             ->assertSee($newsItem->author);
-}
+        $response = $this->get('/news/'.$newsItem->id);
+        $response->assertStatus(200)
+                ->assertSee($newsItem->title)
+                ->assertSee($newsItem->author);
+    }
 
-/** @test */
-public function a_user_can_view_edit_form_for_a_news_item()
-{
-    $newsItem = News::create([
-        'title' => 'Sample News',
-        'resume' => 'This is a sample news item.',
-        'author' => 'Author Name',
-    ]);
+    /** @test */
+    public function a_user_can_view_edit_form_for_a_news_item()
+    {
+        $newsItem = News::create([
+            'title' => 'Sample News',
+            'resume' => 'This is a sample news item.',
+            'author' => 'Author Name',
+        ]);
 
-    $response = $this->get('/news/'.$newsItem->id.'/edit');
-    $response->assertStatus(200)
-             ->assertSee($newsItem->title)
-             ->assertSee($newsItem->author);
-}
+        $response = $this->get('/news/'.$newsItem->id.'/edit');
+        $response->assertStatus(200)
+                ->assertSee($newsItem->title)
+                ->assertSee($newsItem->author);
+    }
 
-/** @test */
-public function a_user_can_update_a_news_item()
-{
-    $newsItem = News::create([
-        'title' => 'Original Title',
-        'resume' => 'Original summary.',
-        'author' => 'Original Author',
-    ]);
+    /** @test */
+    public function a_user_can_update_a_news_item()
+    {
+        $newsItem = News::create([
+            'title' => 'Original Title',
+            'resume' => 'Original summary.',
+            'author' => 'Original Author',
+        ]);
 
-    $this->put('/news/'.$newsItem->id, [
-        'title' => 'Updated Title',
-        'resume' => 'Updated summary.',
-        'author' => 'Updated Author',
-    ])->assertRedirect('/news');
+        $this->put('/news/'.$newsItem->id, [
+            'title' => 'Updated Title',
+            'resume' => 'Updated summary.',
+            'author' => 'Updated Author',
+        ])->assertRedirect('/news');
 
-    $this->assertDatabaseHas('news', [
-        'id' => $newsItem->id,
-        'title' => 'Updated Title',
-        'author' => 'Updated Author',
-    ]);
-}
+        $this->assertDatabaseHas('news', [
+            'id' => $newsItem->id,
+            'title' => 'Updated Title',
+            'author' => 'Updated Author',
+        ]);
+    }
 
-/** @test */
-public function a_user_can_delete_a_news_item()
-{
-    $newsItem = News::create([
-        'title' => 'Sample News',
-        'resume' => 'This is a sample news item.',
-        'author' => 'Author Name',
-    ]);
+    /** @test */
+    public function a_user_can_delete_a_news_item()
+    {
+        $newsItem = News::create([
+            'title' => 'Sample News',
+            'resume' => 'This is a sample news item.',
+            'author' => 'Author Name',
+        ]);
 
-    $this->delete('/news/'.$newsItem->id)
-         ->assertRedirect('/news');
+        $this->delete('/news/'.$newsItem->id)
+            ->assertRedirect('/news');
 
-    $this->assertDatabaseMissing('news', ['id' => $newsItem->id]);
-}
-
+        $this->assertDatabaseMissing('news', ['id' => $newsItem->id]);
+    }
 }
